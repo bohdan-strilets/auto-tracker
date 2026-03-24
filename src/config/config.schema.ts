28b62335fba45427014d9396c49e29652bb48b5c
BS <bohdan.strilets@gmail.com>
2026-03-24 15:26:11 +0100

    feat(config): add options for argon2

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,5 +3,11 @@ import z from 'zod';
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['development', 'production', 'test']),
+
   DATABASE_URL: z.url(),
+
+  ARGON2_MEMORY_COST: z.coerce.number().min(1).max(1048576).default(65536),
+  ARGON2_TIME_COST: z.coerce.number().min(1).default(3),
+  ARGON2_PARALLELISM: z.coerce.number().min(1).default(4),
+  ARGON2_PEPPER: z.string().min(1).max(256),
 });
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -11,15 +11,30 @@ export class ConfigService {
     this.env = envSchema.parse(process.env);
   }
 
+  // App
   get port(): number {
     return this.env.PORT;
   }
-
   get nodeEnv(): string {
     return this.env.NODE_ENV;
   }
 
+  // Database
   get databaseUrl(): string {
     return this.env.DATABASE_URL;
+  }
+
+  // Argon2
+  get argon2MemoryCost(): number {
+    return this.env.ARGON2_MEMORY_COST;
+  }
+  get argon2TimeCost(): number {
+    return this.env.ARGON2_TIME_COST;
+  }
+  get argon2Parallelism(): number {
+    return this.env.ARGON2_PARALLELISM;
+  }
+  get argon2Pepper(): string {
+    return this.env.ARGON2_PEPPER;
   }
 }
