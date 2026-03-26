22d6330ce0d47800bfa39d405c7cebe5323b3a0c
BS <bohdan.strilets@gmail.com>
2026-03-26 16:00:17 +0100

    feat(password): add validation for passwrod

--- src/common/crypto/services/password.service.ts
+++ src/common/crypto/services/password.service.ts
@@ -13,4 +13,30 @@ export class PasswordService {
   verify(hash: string, password: string): Promise<boolean> {
     return this.crypto.verifyArgon2(hash, password);
   }
+
+  // Validation
+
+  hasMinLength(password: string): boolean {
+    return password.length >= 8;
+  }
+
+  hasMaxLength(password: string): boolean {
+    return password.length <= 64;
+  }
+
+  hasUpperCase(password: string): boolean {
+    return /[A-Z]/.test(password);
+  }
+
+  hasLowerCase(password: string): boolean {
+    return /[a-z]/.test(password);
+  }
+
+  hasNumber(password: string): boolean {
+    return /\d/.test(password);
+  }
+
+  isStrong(password: string): boolean {
+    return this.hasUpperCase(password) && this.hasLowerCase(password) && this.hasNumber(password);
+  }
 }
