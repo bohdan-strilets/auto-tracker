7124d7f5701bb1df45a216d216bd4d7cd7378c62
BS <bohdan.strilets@gmail.com>
2026-03-30 13:10:14 +0200

    feat(swagger): add new api decorators

--- src/common/swagger/api-responses.ts
+++ src/common/swagger/api-responses.ts
@@ -133,3 +133,33 @@ export const ApiRevokeSessionResponse = () =>
     ApiNotFoundResponse('SESSION_NOT_FOUND'),
     ApiTooManyRequestsResponse(),
   );
+
+// ─── Invite ──────────────────────────────────────────────────────────────────
+
+export const ApiSendInviteResponse = () =>
+  applyDecorators(
+    ApiValidationResponse(),
+    ApiUnauthorizedResponse(),
+    ApiForbiddenResponse('INSUFFICIENT_PERMISSIONS'),
+    ApiConflictResponse('INVITE_ALREADY_EXISTS'),
+  );
+
+export const ApiGetInvitesResponse = () =>
+  applyDecorators(ApiUnauthorizedResponse(), ApiForbiddenResponse('NOT_WORKSPACE_MEMBER'));
+
+export const ApiCancelInviteResponse = () =>
+  applyDecorators(
+    ApiUnauthorizedResponse(),
+    ApiForbiddenResponse('INSUFFICIENT_PERMISSIONS'),
+    ApiNotFoundResponse('INVITE_NOT_FOUND'),
+  );
+
+export const ApiAcceptInviteResponse = () =>
+  applyDecorators(
+    ApiUnauthorizedResponse(),
+    ApiNotFoundResponse('INVITE_NOT_FOUND'),
+    ApiConflictResponse('WORKSPACE_MEMBER_ALREADY_EXISTS'),
+  );
+
+export const ApiRejectInviteResponse = () =>
+  applyDecorators(ApiNotFoundResponse('INVITE_NOT_FOUND'));
